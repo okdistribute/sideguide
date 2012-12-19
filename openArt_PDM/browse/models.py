b4561ffdc92c15d02337f7fa60d3413fb47bb2f7
Karissa McKelvey <krmckelv@gmail.com>
2012-12-19 00:25:45 -0500

    remove tag from model for simplicity first

--- openArt_PDM/browse/models.py
+++ openArt_PDM/browse/models.py
@@ -10,7 +10,6 @@ class Collection(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True) 
     user = EmbeddedModelField('User')
-    tags = ListField(EmbeddedModelField('Tag'))
 
 class Item(models.Model):
     name = models.CharField(max_length=45)
@@ -21,18 +20,4 @@ class Item(models.Model):
     last_modified = models.DateTimeField(auto_now=True) 
     collection_id = models.IntegerField() 
     coperanda = ListField(EmbeddedModelField('Item'))
-    tags = ListField(EmbeddedModelField('Tag'))
-
-class Tag(models.Model):
-    text = models.CharField(unique=True, max_length=24)
-    
-
-
-
-
-
-
-
-    
-    
 
