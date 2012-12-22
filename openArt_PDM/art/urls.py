4acc7e60ee13dab0be69f7c97d058eb918fe5ac8
Karissa McKelvey <krmckelv@gmail.com>
2012-12-21 19:12:18 -0800

    add urls for art

---
+++ openArt_PDM/art/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
+from django.contrib.auth import views as auth_views
+
+urlpatterns = patterns('',
+    url(r'^items$', 'art.views.items.getItems'),
+    url(r'^item$', 'art.views.items.getItem'),
+)
