a985eb8e4f39fc225def0330b1c31502942a6b19
Karissa McKelvey <krmckelv@gmail.com>
2012-12-22 10:33:26 -0800

    started the user browse view

--- openArt_PDM/browse/views.py
+++ openArt_PDM/browse/views.py
@@ -7,15 +7,25 @@ from django.contrib.auth.decorators import login_required
 from django.template import Context, Template
 from django.utils import simplejson
 from django.contrib.auth.models import User
+from django.db.models import Q
 from art.models import Item, Collection
 
 def user(request, username):
+    """Shows all collections for a given user."""
     c = {}
-    return render_to_response('browse/user.html', c)
+    user = User.objects.get(username__iexact=username)
+    c["user"] = user
 
+    q = Q(user__iexact=username)
+    colls = Collection.objects.filter(q)
+    print colls
+    c["colls"] = colls 
+
+    return render_to_response('browse/user.html', c)
 
 def collection(request, username, collection):
     c = {}
+    collection = Collection.objects.get(user__username__iexact=username)
     return render_to_response('browse/collection.html', c)
 
 
