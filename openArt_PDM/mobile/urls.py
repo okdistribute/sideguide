f18282675e9d566467d28194574284de7405e7c5
Karissa McKelvey <krmckelv@gmail.com>
2012-12-29 15:00:41 -0800

    added mobile demo steps

--- openArt_PDM/mobile/urls.py
+++ openArt_PDM/mobile/urls.py
@@ -5,4 +5,10 @@ from django.contrib.auth import views as auth_views
 urlpatterns = patterns('',
     url(r'^$', 'mobile.views.index',
         {"template_name": "mobile/index.html"}),
+    url(r'^ajax/(?P<username>\w+)$', 'browse.views.view',
+        {"template_name": "mobile/user.html"}),
+    url(r'^ajax/(?P<username>\w+)/(?P<coll_id>\w+)$', 'browse.views.view',
+        {"template_name": "mobile/collection.html"}),
+    url(r'^ajax/(?P<username>\w+)/(?P<coll_id>\w+)/(?P<item_id>\w+)$','browse.views.view', 
+        {"template_name": "mobile/item.html"}),
  )
