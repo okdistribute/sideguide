d1f948f96ba5cab8ecf8a548c9f22b45cf32d7a4
Karissa McKelvey <krmckelv@gmail.com>
2013-03-14 18:14:41 -0400

    use render instead of render_to_response

--- sideguide/registration/views.py
+++ sideguide/registration/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from sideguide.registration.models import RegistrationForm, ActivationProfile
 
-def register(request, 
-    template_name='registration/register.html',
-    post_change_redirect='complete'):
+def register(request, template_name='registration/register.html',  post_change_redirect='complete'):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
@@ -15,19 +13,18 @@ def register(request,
     else:
         form = RegistrationForm()
     
-    return render_to_response(template_name, 
+    return render(request,template_name,
         { 'form': form },
         context_instance=RequestContext(request))
 
 @login_required
 def profile(request):
-    return render_to_response('registration/profile.html', 
-        context_instance=RequestContext(request))
+    return render(request,'registration/profile.html')
 
 def activate(request, activation_key,
     template_name='registration/activate.html'):
     activation_key = activation_key.lower()
     if ActivationProfile.activate_user(activation_key):
-        return render_to_response('registration/activation_successful.html')
+        return render(request,'registration/activation_successful.html')
     else:
-        return render_to_response('registration/error.html')
+        return render(request,'registration/error.html')
