9a9e3f48d14a3ebf1abd71f4dad7a29a43d20b9f
Karissa McKelvey <krmckelv@gmail.com>
2013-03-14 18:13:06 -0400

    fixed up some views

--- sideguide/browse/views/curate.py
+++ sideguide/browse/views/curate.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from sideguide.common.forms import CollForm
 
 def index(request):
     """/curate"""
     c = {}
-    return render_to_response("browse/curate.html", c)
+    return render(request, "browse/curate.html", c)
 
 def dest(request):
     """/curate/desintation-name
     :returns a view of that destination for curation"""
 
     c = {}
-    return render_to_response("browse/")
+    return render(request, "browse/curate.html", c)
+
+@require_http_methods(["POST"])
+def create_coll(request):
+    """/curate
+    creates a collection"""
+    form = CollForm(request)
+    c = {}
+    return render(request, "browse/curate.html", c)
--- sideguide/browse/views/explore.py
+++ sideguide/browse/views/explore.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 def index(request):
     c = {}
-    return render_to_response("browse/explore.html", c)
+    return render(request, "browse/explore.html", c)
 
 def dest(request):
     """/browse/destinationname
     :returns a view of that destination"""
 
     c = {}
-    return render_to_response("browse/")
+    return render(request, "browse/", c)
